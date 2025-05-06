2f9ea33c71c5416569825500d99a99d223a20ae3
gavinarori <arorigavin@gmail.com>
2025-05-06 19:58:28 +0300

    added: payment reducer

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -2,11 +2,13 @@ import homeReducer from './reducers/homeReducer'
 import authReducer from './reducers/authReducer'
 import cardReducer from './reducers/cardReducer'
 import chatReducer from './reducers/chatReducer'
+import paymentReducer from './reducers/paymentReducer'
 
 const rootReducers = {
     home: homeReducer,
     auth: authReducer,
     card: cardReducer,
     chat : chatReducer,
+    payment: paymentReducer
 }
 export default rootReducers
